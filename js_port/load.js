a9e62a12bdd02d4d5d417b7c1d59c686025de163
Buggem's Fedora PC <66670928+Buggem@users.noreply.github.com>
2024-11-26 19:43:28 +1100

    Add more archived files and begin work on the JavaScript port

---
+++ js_port/load.js
@@ -0,0 +1,18 @@
+
+
+function setup()
+{
+ // ... loading of assets, etc
+ startup();
+}
+
+
+function visit_distractionware() //unused until i reimplement context menu
+{
+ window.open("http://www.distractionware.com");
+}
+
+function startup() {
+ Main();
+}
+
